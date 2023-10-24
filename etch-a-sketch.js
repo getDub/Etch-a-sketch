caa0ea5aa472911ee3fcb7a6b8edef9741d19bf0
getDub <143089345+getDub@users.noreply.github.com>
2023-10-24 15:41:35 +1100

    Creates grid and cells change colour when mouse hovers over cells.
    Totally new direction in the code using css grid and inline-grid to create the grid rather than flex containers.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,38 +1,18 @@
 const mainContainer = document.querySelector('.mainContainer');
-const columns = document.createElement('div');
-const rows = document.createElement('div');
-const rowUnits = document.createElement('div');
-console.log(rows.classList)
 
 
-columns.classList.add('columns');
-mainContainer.appendChild(columns);
-
-
-
-for(let i = 1; i < 16; i++){
-    const rows = document.createElement('div');
-    rows.classList.add('rows');
-    columns.appendChild(rows);
-    for(let i = 1; i < 16; i++){
-        const rowUnits = document.createElement('div');
-        rowUnits.classList.add('rowUnits');
-        rowUnits.setAttribute('class', 'rowUnits');
-        rows.appendChild(rowUnits);
+//Create gird
+for(let i = 0; i < 256; i++){
+    //Create div element and call it cell
+    const cell = document.createElement('div');
+    //Give the div a class named cell.
+    cell.classList.add('cell');
+    //Append the cell div to the mainContainer div
+    mainContainer.appendChild(cell);
+    //Apply a mouseover event listener to the cell div with a callback function named cellColour
+    cell.addEventListener('mouseover', cellColour);
+    //Create a function to add another class name to the cell div. This new class is in the CSS style with a different background color to the default.
+    function cellColour(){
+        cell.classList.add('cellColourChange');
     }
 }
-
-
-function mouseOver(){
-    rowUnits.setAttribute('style', 'backgroundColor: red;');
-}
-
-for(i=0; i < rowUnits.length; ++i){
-    rowUnits[i].addEventListener("click", mouseOver)
-    console.log('yes');
-}
-
-
-// rowUnits.forEach((units) => {
-//     units.addEventListener("click", mouseOver)
-// });
